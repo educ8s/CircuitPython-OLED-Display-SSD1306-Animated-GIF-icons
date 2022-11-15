5d8451bec16a6135ca96bfad5c19eb595e872f23
educ8s <info@educ8s.tv>
2022-11-15 09:25:36 +0200

    video code

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-# This script supports only the Raspberry Pi Pico board
 # Raspberry Pi Pico: http://educ8s.tv/part/RaspberryPiPico
 # OLED DISPLAY: https://educ8s.tv/part/OLED096
 
@@ -6,7 +5,8 @@ import board, busio, displayio, time
 import adafruit_displayio_ssd1306
 import adafruit_imageload
 
-IMAGE_FILE = "icon.bmp"
+IMAGE_FILE = "youtube.bmp"
+SPRITE_SIZE = (64, 64)
 FRAMES = 28
 
 def invert_colors():
@@ -17,8 +17,8 @@ def invert_colors():
 displayio.release_displays()
 
 sda, scl = board.GP0, board.GP1
- 
 i2c = busio.I2C(scl, sda)
+
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
@@ -32,7 +32,7 @@ invert_colors()
 
 icon_grid = displayio.TileGrid(icon_bit, pixel_shader=icon_pal,
                                  width=1, height=1,
-                                 tile_height=64, tile_width=64,
+                                 tile_height=SPRITE_SIZE[1], tile_width=SPRITE_SIZE[0],
                                  default_tile=0,
                                  x=32, y=0)
 
